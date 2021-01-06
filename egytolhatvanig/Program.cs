a97bdde2efb0e79f06f83d38617e1dec2c3cad59
Alphex23 <31734028+TirvolCsaba@users.noreply.github.com>
2021-01-06 08:43:01 +0100

    Add project files.

---
+++ egytolhatvanig/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace egytolhatvanig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       //   //1.1
+       //   Console.WriteLine("hajrá Fradi");
+       //   //1.2
+       //   Console.WriteLine("5");
+       //   //1.3
+       //   Console.WriteLine("5.4");
+       //   //1.4
+       //   Console.WriteLine('*');
+       //   //2
+            //2.1
+            string azonosito = "szo";
+            //2.2
+            int x;
+            //2.3
+            double y;
+            //2.4
+            char k;
+            //2.5
+            bool l = true;
+            //4
+       //   Console.WriteLine("adja meg a nevét!");
+       //   azonosito = Console.ReadLine();
+       //   Console.WriteLine("jó napot "+azonosito);
+       //   //5
+       //   Console.WriteLine("mi a kedvenc foci csapata");
+       //   azonosito = Console.ReadLine();
+       //   Console.WriteLine("Hajrá "+azonosito);
+       //   //6
+       //   Console.WriteLine("adjon értéket");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(x*2);
+       //   Console.WriteLine(x/2);
+       //   Console.WriteLine(x+1);
+       //   Console.WriteLine(x-3);
+       //   Console.WriteLine(50/x);
+       //   Console.WriteLine(10%x);
+       //   Console.WriteLine(x^2);
+       //   //7
+       //   Console.WriteLine("két egész számot adjon");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   y = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(x*y);
+       //   Console.WriteLine(x%y);
+       //   Console.WriteLine(x/y);
+       //   if (x>y)
+       //   {
+       //       Console.WriteLine(x+" is higher");
+       //   }
+       //   if (y>x)
+       //   {
+       //       Console.WriteLine(y + " is higher");
+       //   }
+       //   //8
+       //   Console.WriteLine("adja meg a négyzet oldalait (egész számmal)");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(x*x+" a területe");
+       //   Console.WriteLine(x*4+" a kerülete");
+       //   //9
+       //   Console.WriteLine("adja meg a téglalap oldalait (egész számmal)");
+       //   y = Convert.ToInt32(Console.ReadLine());
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(x * y + " a területe");
+       //   Console.WriteLine(2*(x+y)+"a kerülete");
+       //   //10
+       //   Console.WriteLine("adja meg a háromszög oldalait (egész számmal) és az első az A oldal utolsó pedig a magasság");
+       //   int harmadik;
+       //   int magassag;
+       //   y = Convert.ToInt32(Console.ReadLine());
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   harmadik = Convert.ToInt32(Console.ReadLine());
+       //   magassag = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine("a háromszög területe= "+(magassag*y)/2);
+       //   Console.WriteLine("kerülete "+x+y+harmadik);
+       //   //11
+       //   Console.WriteLine("adja meg a kör sugarát (egész számmal)");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine("területe " + (Math.PI*x)* (Math.PI * x));
+       //   Console.WriteLine("kerülete " + 2*(Math.PI*x)   );
+       //   //12
+       //   Console.WriteLine("1kg krumpli 210ft mennyi krumplit óhajt?");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine("ennyibe fog fájni:"+x*210+"FT");
+       //   //13
+       //   Console.WriteLine("mennyi eurót szeretne vásárloni? 362ft az árfolyam 360ft-os vétel aránnyal");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(362*x+" ft ba fog kerülni");
+       //   Console.WriteLine("most mondja el mennyi eurója van");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine(x*360+" ft-nyi eurója van");
+       //   //14
+       //   Console.WriteLine("megmondom milyen gyorsan mész ha megmondod mennyi utat tettél meg mennyi idő alatt(másodpercben)");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   y = Convert.ToInt32(Console.ReadLine());
+       //   Console.WriteLine("a sebessége m/s="+x/y);
+       //   //15
+       //   Console.WriteLine("hány éves?");
+       //   x = Convert.ToInt32(Console.ReadLine());
+       //   if (x>=18&&65>=x)
+       //   {
+       //       Console.WriteLine("ön felnőtt");
+       //   }
+       //   else if (x>65)
+       //   {
+       //       Console.WriteLine("ön nyugdijas");
+       //   }
+       //   else if (14 >= x)
+       //   {
+       //       Console.WriteLine("ön fiatalkorú");
+       //   }
+            //16
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
